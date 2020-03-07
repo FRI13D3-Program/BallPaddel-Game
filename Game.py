15b750eeadde43009b72d4da41f54b96f0d463f9
FRI13D3-Program <61900453+FRI13D3-Program@users.noreply.github.com>
2020-03-07 18:05:00 +0000

    Update Game.py

--- Game.py
+++ Game.py
@@ -10,17 +10,14 @@ ball_x, ball_y = random.randrange(0, 900), random.randrange(0, 80)
 directionBall_X, directionBall_Y = 1, 1
 damageNum = 3
 score = 0
-# end set sizes
 
 # start board icon
 board = pygame.image.load('board.jpg')
 boardSize = board.get_size()
-# end board icon
 
 # start ball icon
 ball = pygame.image.load('ball.jpg')
 ballSize = ball.get_size()
-# end ball icon
 
 calibri = pygame.font.SysFont('Calibri', 21)
 Damage = calibri.render("health : ", 1, (225, 0, 0), (0, 0, 0))
@@ -35,14 +32,12 @@ pygame.mouse.set_visible(0)
 
 # start get time
 clock = pygame.time.Clock()
-# start get time
 
 # start say Game Over and restart game
 def GameOver():
     playsound('GameOver.mp3')
     time.sleep(2)
     Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y, damageNum, score)
-# end say Game Over and restart game
 
 # start start Game Loop
 def Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y, damageNum, score):
@@ -51,7 +46,6 @@ def Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y
         # start set speed
         randTimeSecond = random.randrange(50, 350)
         clock.tick(randTimeSecond)
-        # end set speed
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -59,7 +53,6 @@ def Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y
 
         # start style
         screen.fill((0, 0, 0))
-        # end style
 
         # start get mouse position
         mousePosition = pygame.mouse.get_pos()
@@ -67,26 +60,22 @@ def Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y
         # start set mouse position
         board_x, board_y = mousePosition
         board_y = 560
-        # end set mouse position
 
         # start board if
         if board_x + boardSize[0] > 900:
             board_x = 900 - boardSize[0]
-        # end board if
 
         # start ball mover
         #X_mover = random.randint(4, 8)
         #Y_mover = random.randint(4, 7)
         ball_x += 6 * directionBall_X
         ball_y += 6 * directionBall_Y
-        # end ball mover
 
         # start ball if
         if ball_x + ballSize[0] > 900 or ball_x <= 0:
             directionBall_X *= -1
         if ball_y + ballSize[1] > 600 or ball_y <= 0:
             directionBall_Y *= -1
-        # end ball if
 
         # start gameOver if
         if ball_y + ballSize[1] > 600:
@@ -94,7 +83,6 @@ def Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y
                 GameOver()
             else:
                 damageNum -= 1
-        # end gameOver if
 
         # start Computing damage
         if damageNum == 3:
@@ -103,7 +91,6 @@ def Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y
             health = calibri.render("2", 1, (225, 0, 0), (0, 0, 0))
         if damageNum == 1:
             health = calibri.render("1", 1, (225, 0, 0), (0, 0, 0))
-        # end Computing damage
 
         # start Reaction and calculate ball and board
         if ball_y + ball_x > board_y + board_x:
@@ -113,7 +100,6 @@ def Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y
 
             # if score >= 100:
             #     screen.fill((70, 70, 70))
-        # end Reaction and calculate ball and board
 
         # start set ball, board and damage on windows
         if damageNum == 0:
@@ -122,10 +108,9 @@ def Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y
         screen.blit(health, (70, 1))
         screen.blit(board, (board_x, board_y))
         screen.blit(ball, (ball_x, ball_y))
-        # end set ball and board on windows
 
         # start window updater
         pygame.display.update()
+        
 # start start Game Loop
-
 Game_Loop(board_x, board_y, ball_x, ball_y, directionBall_X, directionBall_Y, damageNum, score)
